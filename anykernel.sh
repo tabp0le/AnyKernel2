dfe0a0d1832613a655f75a982728183232f4bc61
Tab Fitts <tfitts1212@gmail.com>
2018-01-09 14:41:41 -0700

    Don't ship ramdisk files anymore. Max compatibility

--- anykernel.sh
+++ anykernel.sh
@@ -31,27 +31,12 @@ is_slot_device=0;
 ## AnyKernel permissions
 # set permissions for included ramdisk files
 chmod -R 755 $ramdisk
-chmod 640 $ramdisk/fstab.qcom
-
-
 
 ## AnyKernel install
 dump_boot;
-
-# begin ramdisk changes
-backup_file init.qcom.power.rc;
-replace_file init.qcom.power.rc 0755 init.qcom.power.rc;
-
-backup_file init.qcom.power.rc;
-replace_file init.qcom.rc 0755 init.qcom.rc;
-
-# fstab
-backup_file fstab.qcom;
-replace_file fstab.qcom 0640 fstab.qcom;
 
 # end ramdisk changes
 
 write_boot;
 
 ## end install
-
